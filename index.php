859796c36ac51ed24c87e011964d96cf09e01e2c
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:05:40 +0100

    fini

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice2</title>
+  </head>
+  <body>
+    <?php
+    $second = 5;
+    $first = 0;
+    while($first <= 20){
+      $first++;
+      $result = ($first * $second);
+      echo $result.'<br />';
+    }
+     ?>
+
+  </body>
+</html>
